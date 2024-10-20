a64fcbfae1c15b826ff5a56b4dd2b64601ad60c0
Fererra <126345645+Fererra@users.noreply.github.com>
2024-10-20 12:17:37 +0300

    fix the tests

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -13,16 +13,16 @@ const rangeOdd = (start, end) => {
 };
 
 // Tests from 5-range-odd.test
-// console.log(range(0, 1)); // Output: [1]
-// console.log(range(0, 5)); // Output: [1, 3, 5]
-// console.log(range(0, 6)); // Output: [1, 3, 5]
-// console.log(range(1, 3)); // Output: [1, 3]
-// console.log(range(-2, 2)); // Output: [-1, 1]
-// console.log(range(-3, -1)); // Output: [-3, -1]
-// console.log(range(0, 0)); // Output: []
-// console.log(range(5, 5)); // Output: [5]
-// console.log(range(-1, -1)); // Output: [-1]
-// console.log(range(7, 5)); // Output: []
-// console.log(range(2, -1)); // Output: []
+// console.log(rangeOdd(0, 1)); // Output: [1]
+// console.log(rangeOdd(0, 5)); // Output: [1, 3, 5]
+// console.log(rangeOdd(0, 6)); // Output: [1, 3, 5]
+// console.log(rangeOdd(1, 3)); // Output: [1, 3]
+// console.log(rangeOdd(-2, 2)); // Output: [-1, 1]
+// console.log(rangeOdd(-3, -1)); // Output: [-3, -1]
+// console.log(rangeOdd(0, 0)); // Output: []
+// console.log(rangeOdd(5, 5)); // Output: [5]
+// console.log(rangeOdd(-1, -1)); // Output: [-1]
+// console.log(rangeOdd(7, 5)); // Output: []
+// console.log(rangeOdd(2, -1)); // Output: []
 
 module.exports = { rangeOdd };
